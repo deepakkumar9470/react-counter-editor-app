2f2b6b9558a65352c3b79bb1c8c428ac21b65e6f
deepakkumar9470 <dk397787@gmail.com>
2025-02-05 10:48:32 +0530

    fixing unused state on vercel

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,6 @@
 
 import { useAppSelector } from "@/store/hooks/hooks";
 import { RootState } from "@/store/store";
-import { Button } from "@chakra-ui/react";
 import QuillEditor from "react-quill";
 import "react-quill/dist/quill.snow.css";
 const Editor = ({ value, onChange }: { value: string, onChange: React.Dispatch<string> }) => {
